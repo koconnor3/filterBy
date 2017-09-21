528069c9406a5af30d4dd2ae2af4a6573edcc2f8
Kevin O'Connor <kevpedia@gmail.com>
2017-09-21 13:54:16 -0400

    fixes: "Hides Story if I'm Not the Sole Owner"
    Now shows stories where I am one of multiple owners as well
    [fixes #150712533]

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -21,12 +21,21 @@ chrome.extension.sendMessage({}, function(response) {
         var storyElements = document.querySelectorAll(".preview");
         storyElements.forEach(function(story){
           var storyOwnerElements = story.querySelectorAll(".owner");
+            var ownersToDisplay = [];
             storyOwnerElements.forEach(function(owner){
-              if (owner.title!="Kevin O'Connor") {
-                  console.log("Story Owner not Kevin O'Connor, Hiding Story");
-                  story.parentNode.style.display = 'none'
-                }
+              if (owner.title=="Kevin O'Connor") {
+                  ownersToDisplay.push(owner.title);
+              }
             });
+            if (ownersToDisplay.length == 0) {
+              story.parentNode.style.display = 'none';
+            }
+            // storyOwnerElements.forEach(function(owner){
+            //   if (owner.title!="Kevin O'Connor") {
+            //       console.log("Story Owner not Kevin O'Connor, Hiding Story");
+            //       story.parentNode.style.display = 'none'
+            //   }
+            // });
           });
         }
       }
