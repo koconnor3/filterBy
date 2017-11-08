34c9e9e3ab1b62f8852041f6e56e841d74ad34e5
Kevin O'Connor <kevpedia@gmail.com>
2017-11-08 13:02:01 -0500

    Options Page Allows User to Input Initials to Filter By
    [delivers #150712756]

--- hideStories.js
+++ hideStories.js
@@ -1,20 +1,31 @@
-var storyElements = document.querySelectorAll(".preview");
-	console.log('found '+storyElements.length+'stories');
-    storyElements.forEach(function(story){
-      var storyOwnerElements = story.querySelectorAll(".owner");
-        var ownersToDisplay = [];
-        storyOwnerElements.forEach(function(owner){
-          if (owner.title=="Kevin O'Connor") {
-              ownersToDisplay.push(owner.title);
+var name = "Kevin O'Connor"; //document.getElementsByClassName("AvatarModal__name")[0].innerHTML;
+
+chrome.storage.sync.get({
+    initials: 'KO'
+  }, function(items) {
+    var initials = items.initials;
+    console.log("Current User: '"+initials+"'");
+    hideStories(initials);
+  });
+
+function hideStories(initials) {
+  var storyElements = document.querySelectorAll(".preview");
+      storyElements.forEach(function(story){
+        var storyOwnerElements = story.querySelectorAll(".owner");
+          var ownersToDisplay = [];
+          storyOwnerElements.forEach(function(owner){
+            if (owner.innerHTML==initials) {
+                ownersToDisplay.push(owner.title);
+            }
+          });
+          if (ownersToDisplay.length == 0) {
+            story.parentNode.style.display = 'none';
           }
+          // storyOwnerElements.forEach(function(owner){
+          //   if (owner.title!="Kevin O'Connor") {
+          //       console.log("Story Owner not Kevin O'Connor, Hiding Story");
+          //       story.parentNode.style.display = 'none'
+          //   }
+          // });
         });
-        if (ownersToDisplay.length == 0) {
-          story.parentNode.style.display = 'none';
-        }
-        // storyOwnerElements.forEach(function(owner){
-        //   if (owner.title!="Kevin O'Connor") {
-        //       console.log("Story Owner not Kevin O'Connor, Hiding Story");
-        //       story.parentNode.style.display = 'none'
-        //   }
-        // });
-      });
+}
